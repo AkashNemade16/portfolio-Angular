8c0eb6708d164aca6f4db8a79eab2c1a94b13b8a
akash <aksh.akash@gmail.com>
2023-05-24 19:25:14 -0500

    icons added

--- src/app/Header/Header.component.ts
+++ src/app/Header/Header.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {faLinkedin, faGithub, faTwitter} from '@fortawesome/free-brands-svg-icons'
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./Header.component.sass']
 })
 export class HeaderComponent {
-
+  faLinkedin = faLinkedin
+  faGithub = faGithub
+  faTwitter = faTwitter
 }
