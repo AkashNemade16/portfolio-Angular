8b02956974a6dfabcb1772954b0d430c33bdba4c
akash <aksh.akash@gmail.com>
2023-05-24 23:37:57 -0500

    skills

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './Header/header.component';
+import { HeaderComponent } from '../Header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SkillsComponent } from '../skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
