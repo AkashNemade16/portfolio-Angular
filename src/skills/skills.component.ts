7127c241556af221dbc44aef0d9eec174458eedf
akash <aksh.akash@gmail.com>
2023-05-30 17:33:47 -0500

    data restructuring

--- src/skills/skills.component.ts
+++ src/skills/skills.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
-
+import { Skills } from './skillsInterface';
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.sass']
 })
 export class SkillsComponent {
-
+  skillList:Skills[] = [
+    {
+      skill:"HTML",
+      experience:"4 years"
+    },
+    {
+      skill:"CSS",
+      experience:"4 years"
+    },
+    {
+      skill:"ReactJs",
+      experience:"4 years"
+    },
+    {
+      skill:"Accessibility",
+      experience:"4 years"
+    },
+    {
+      skill:"Angular",
+      experience:"1 year"
+    },
+  ]
 }
