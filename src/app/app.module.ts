a174a9be66e3bca358631daa597a222594a02cdc
akash <aksh.akash@gmail.com>
2023-05-30 22:15:32 -0500

    responsiveness

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from '../Header/header.component';
+import { HeaderComponent } from '../Header/Header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SkillsComponent } from '../skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SkillsComponent
+    SkillsComponent,
+    ProjectsComponent,
   ],
   imports: [
     BrowserModule,
