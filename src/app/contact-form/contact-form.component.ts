fa70e31caa94e3fa8c71ba80a77fa6a1f2d7f043
akash <aksh.akash@gmail.com>
2023-06-01 12:47:26 -0500

    layout complete

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import {faLinkedin, faGithub, faTwitter} from '@fortawesome/free-brands-svg-icons'
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.sass']
 })
 export class ContactFormComponent {
-
+  faLinkedin = faLinkedin
+  faGithub = faGithub
+  faTwitter = faTwitter
 }
