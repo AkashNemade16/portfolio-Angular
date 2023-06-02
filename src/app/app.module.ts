c99d90c1cf9c898ae6f72b53543f621515d1b840
akash <aksh.akash@gmail.com>
2023-06-01 22:38:05 -0500

    server set with frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from '../Header/Header.component';
@@ -20,7 +20,8 @@ import { ContactFormComponent } from './contact-form/contact-form.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
 import {faLinkedin, faGithub, faTwitter} from '@fortawesome/free-brands-svg-icons'
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.sass']
 })
+
 export class ContactFormComponent {
+  constructor(private http:HttpClient){
+
+  }
   faLinkedin = faLinkedin
   faGithub = faGithub
   faTwitter = faTwitter
+  
+
+  sendData =  (name:string,email:string,message:string) => {
+    const user ={
+      name,
+      email,
+      message
+    }
+    console.log(user)
+    if(name!='' && email!='' && message!=''){
+      this.http.post('http://localhost:3001/api/insert',user)
+      .subscribe(res=>console.log(res))
+    }
+  }
+
+  submit=(e:Event)=>{
+    e.preventDefault
+    console.log('hello')
+    this.sendData
+  }
 }
